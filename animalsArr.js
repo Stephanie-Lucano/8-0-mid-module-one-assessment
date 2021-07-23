d2026acc7706bbee0ad7a4539dafd4ffd9e59bce
stephanie lucano <stephaniemarielucano@gmail.com>
2021-07-23 10:28:13 -0400

    Preview the animals array in new file

---
+++ animalsArr.js
@@ -0,0 +1,8 @@
+const animals = [
+    { kind: "Pig", count: 5 },
+    { kind: "Cow", count: 7 },
+    { kind: "Chicken", count: 11 },
+    { kind: "Horse", count: 1 },
+    { kind: "Dog", count: 2 },
+    { kind: "Cat", count: 2 },
+  ];
